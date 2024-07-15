a2bd2c291f328dcffe998b29fd4c37dab027c555
Marius Tobiassen Bungum <marius@mh2.no>
2024-07-15 13:58:06 +0200

    :fire: Removed ACL related code

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,12 +1,5 @@
 import AuthProvider from './AuthProvider/AuthProvider';
 import TutorialProvider from './TutorialProvider/TutorialProvider';
 import FeatureToggleProvider from './FeatureToggleProvider';
-import ReleaseNotesProvider from './ReleaseNotesProvider';
-import TutorialStepsProvider from './TutorialStepsProvider';
-export {
-  AuthProvider,
-  FeatureToggleProvider,
-  TutorialProvider,
-  TutorialStepsProvider,
-  ReleaseNotesProvider,
-};
+
+export { AuthProvider, FeatureToggleProvider, TutorialProvider };
