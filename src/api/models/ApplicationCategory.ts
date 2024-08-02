dd4e66239d9626d8ce6c841a8cd4a70746c7b17b
Marius Tobiassen Bungum <marius@mh2.no>
2024-08-02 12:09:09 +0200

    :alien: Added missing API types needed for AmplifyApplication.ts

---
+++ src/api/models/ApplicationCategory.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export enum ApplicationCategory {
+    JSDigitalFrontrunner = 'JS Digital Frontrunner',
+    Upscaling = 'Upscaling',
+    Toolbox = 'Toolbox',
+    External = 'External',
+}
---
+++ src/api/models/Chapter.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Chapter = {
+    order: number;
+    title: string;
+    subTitle: string;
+    content: string;
+};
+
