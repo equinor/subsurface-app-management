c350099843c0849344f482f185830502ade16828
aslakihle <aslak.ihle@gmail.com>
2024-11-06 14:54:38 +0100

    :recycle: Move JSDoc text

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,6 +1,3 @@
 export { TutorialProvider } from './TutorialProvider/TutorialProvider';
 export { FeatureToggleProvider } from './FeatureToggleProvider';
-/**
- * Use with caution. There has not been much testing of how using this hook in an app affects the tutorial logic
- */
 export { useTutorial } from './TutorialProvider/TutorialProvider.hooks';
