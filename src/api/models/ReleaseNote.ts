d7f379e704c54df6cb4b92aae3a0d6b94bc4e3ac
Marius Tobiassen Bungum <marius@mh2.no>
2024-12-18 07:18:50 +0100

    :alien: Update ReleaseNote model to match the one that is in SAM

--- src/api/models/ReleaseNote.ts
+++ src/api/models/ReleaseNote.ts
@@ -3,11 +3,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type ReleaseNote = {
-  releaseId?: string | null;
+  releaseId: string;
   applicationName: string;
   version?: string | null;
   title: string;
   body: string;
   tags?: Array<string> | null;
+  draft: boolean;
   createdDate: string;
+  releaseDate?: string | null;
 };
