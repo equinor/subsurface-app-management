b4922c86b685831fb3c96c65e8747703187a5ec4
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-27 13:56:36 +0100

    :sparkles: Added signalR hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export { useFeatureToggling } from './useFeatureToggling';
 export { useReleaseNotesQuery } from './useReleaseNotesQuery';
 export { useTutorialsQuery } from './useTutorialsQuery';
 export { useTutorialStepImage } from './useTutorialStepImage';
+export { useSignalRMessages } from './useSignalRMessages';
