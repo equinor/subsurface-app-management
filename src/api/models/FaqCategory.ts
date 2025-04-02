218ffe613a08fed2cd98698e993964c2a2c72ef2
Marius Tobiassen Bungum <marius@mh2.no>
2025-04-02 07:39:56 +0200

    :alien: Add 'getCategoriesWithFaqsFromApplicationName' endpoint

--- src/api/models/FaqCategory.ts
+++ src/api/models/FaqCategory.ts
@@ -2,9 +2,11 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import { Faq } from 'src/api';
 export type FaqCategory = {
   id: number;
   categoryName: string;
   orderBy?: number | null;
   applicationId: string;
+  faqs: Array<Faq>;
 };
