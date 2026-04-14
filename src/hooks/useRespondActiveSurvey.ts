38ef8a8571f0b75e846193574300c8f42e7c6132
Marius Tobiassen Bungum <marius@mh2.no>
2026-04-14 12:08:09 +0200

    refactor: Use surveyId from body in respond survey call

--- src/hooks/useRespondActiveSurvey.ts
+++ src/hooks/useRespondActiveSurvey.ts
@@ -7,16 +7,14 @@ import {
   UserSurveyVm,
 } from 'src/api';
 import { GET_SURVEY_FOR_APP } from 'src/constants';
-import { useActiveSurvey } from 'src/hooks/useActiveSurvey';
 
 export function useRespondActiveSurvey() {
-  const { data: activeSurvey } = useActiveSurvey();
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async (body: StartSurveyDto) => {
       const data = await SurveysPublicService.createSurveyResponse(
-        activeSurvey?.surveyId.value ?? '',
+        body.surveyId.value,
         body
       );
       queryClient.setQueryData<UserSurveyVm>(
