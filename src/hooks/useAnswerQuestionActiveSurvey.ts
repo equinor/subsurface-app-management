7bc0af3e15dbc0744b479352f30dfc62b3e3e5aa
Marius Tobiassen Bungum <marius@mh2.no>
2026-04-15 13:58:24 +0200

    fix: Update cache on answer

--- src/hooks/useAnswerQuestionActiveSurvey.ts
+++ src/hooks/useAnswerQuestionActiveSurvey.ts
@@ -1,8 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { AnswerQuestionCommandDto, SurveysPublicService } from 'src/api';
+import {
+  AnswerQuestionCommandDto,
+  SurveysPublicService,
+  UserSurveyVm,
+} from 'src/api';
+import { GET_SURVEY_FOR_APP } from 'src/constants';
 
 export function useAnswerQuestionActiveSurvey() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({
       surveyResponseId,
@@ -11,5 +17,23 @@ export function useAnswerQuestionActiveSurvey() {
       surveyResponseId: string;
       body: AnswerQuestionCommandDto;
     }) => SurveysPublicService.upsertSurveyResponse(surveyResponseId, body),
+    onSuccess: (data, context) => {
+      queryClient.setQueryData<UserSurveyVm>(
+        [GET_SURVEY_FOR_APP],
+        (oldData) => {
+          const copy = structuredClone(oldData);
+
+          const updatedQuestion = copy?.questions.find(
+            (q) => q.questionId.value === context.body.id.value
+          );
+
+          if (updatedQuestion) {
+            updatedQuestion.answer = { ...context.body, answerId: data };
+          }
+
+          return copy;
+        }
+      );
+    },
   });
 }
