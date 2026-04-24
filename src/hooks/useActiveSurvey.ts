f002f56a87671e00494b662e2dede4e7a710fb9f
Marius Tobiassen Bungum <marius@mh2.no>
2026-04-24 07:40:09 +0200

    fix: Sorting multiple choice options

--- src/hooks/useActiveSurvey.ts
+++ src/hooks/useActiveSurvey.ts
@@ -22,9 +22,15 @@ export function useActiveSurvey() {
           .toSorted((a, b) => a.order - b.order)
           .map((question) => ({
             ...question,
-            options: question.multipleChoiceVm?.options.toSorted(
-              (a, b) => a.order - b.order
-            ),
+            multipleChoiceVm: question.multipleChoiceVm
+              ? {
+                  maxSelectableOptions:
+                    question.multipleChoiceVm.maxSelectableOptions,
+                  options: question.multipleChoiceVm?.options.toSorted(
+                    (a, b) => a.order - b.order
+                  ),
+                }
+              : undefined,
           })),
       };
     },
